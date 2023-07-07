a913022456676b9cfd9df131e0b0ca0552425588
Junnos  <junnos.dev@gmail.com>
2023-07-08 01:17:25 +0900

    doc

--- build-docs.sh
+++ build-docs.sh
@@ -5,6 +5,12 @@ xcodebuild docbuild  \
   -derivedDataPath ~/Workspace/DerivedDataFoler \
   -destination 'platform=iOS Simulator,name=iPhone 14'
   
-find ~/Workspace/DerivedDataFoler \
-  -type d \
-  -name "*.doccarchive"
+doccarchive_path=$(find ~/Workspace/DerivedDataFoler -type d -name "*.doccarchive")
+  
+if [[ -z $doccarchive_path ]]; then
+  echo "No doccarchive file found."
+  exit 1
+fi
+  
+$(xcrun --find docc) process-archive transform-for-static-hosting \
+  "$doccarchive_path" --output-path docs
