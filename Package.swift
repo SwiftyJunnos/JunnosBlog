d562df740a90cd81b7600a6a2655e5d5353016e4
Junnos  <junnos.dev@gmail.com>
2023-07-08 17:24:08 +0900

    swift tools version to 5.8

--- Package.swift
+++ Package.swift
@@ -1,16 +1,10 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "JunnosBlog",
-    platforms: [
-      .iOS(.v15),
-      .macOS(.v12),
-      .watchOS(.v8),
-      .tvOS(.v15)
-    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
